9b982d0f155eaf65a35ef458b509122a5f08a0fc
vaandhare <vaibhavandhare7@gmail.com>
2020-05-27 00:23:00 +0530

    Initial commit

---
+++ app/src/main/java/in/birdvision/equibiz/UserInfo/LoginActivity.java
@@ -0,0 +1,32 @@
+package in.birdvision.equibiz.UserInfo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputLayout;
+
+import in.birdvision.equibiz.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    TextView tvRegister;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        tvRegister = findViewById(R.id.tv_register);
+
+        tvRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
+            }
+        });
+    }
+}
---
+++ app/src/main/java/in/birdvision/equibiz/UserInfo/RegisterActivity.java
@@ -0,0 +1,30 @@
+package in.birdvision.equibiz.UserInfo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import in.birdvision.equibiz.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    TextView tvLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        tvLogin = findViewById(R.id.tv_login);
+
+        tvLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
+            }
+        });
+    }
+}
